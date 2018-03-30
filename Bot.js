5732a794b40c9812b8d49c4094a57dba412c7e1d
王喆 <nasa127@gmail.com>
2018-03-30 23:46:41 +0800

    完善细节

--- Bot.js
+++ Bot.js
@@ -28,23 +28,47 @@ class Bot extends BaseBot {
 
     this.addIntentHandler('choice', () => {
       this.waitAnswer()
-      var choice = this.getSlot('choice')
-      if (choice) {
+      let answer = this.getSlot('answer')
+      let doc = this.getSessionAttribute('doc')
+      if (answer && doc) {
+        let num_a = doc.num_a
+        let num_b = doc.num_b
+        let corps = '多数派'
+        let percent = 0
+        let choise = ''
+        if (answer == 'a') {
+          num_a++
+          corps = num_a > num_b ? '多数派' : '少数派'
+          percent = _.floor(num_a / (num_a + num_b) * 100, 1)
+          choise = doc.a
+        } else {
+          num_b++
+          corps = num_b > num_a ? '多数派' : '少数派'
+          percent = _.floor(num_b / (num_a + num_b) * 100, 1)
+          choise = doc.b
+        }
+        choiceDb
+          .update({ _id: doc._id }, { $set: { num_a: num_a, num_b: num_b } }, { returnUpdatedDocs: true }, (err, numAffected, affectedDocuments) => (err ? console.log(err) : console.log(affectedDocuments)))
         this.endSession()
         return {
-          outputSpeech: `
-            共有46%的人和你一样选择了${choice}
-            `
+          outputSpeech: `共有${percent}%的人和你一样选择了${choise},你是${corps}。`
         }
       } else {
-        return {
-          outputSpeech: `
-            真爱和一千万，您选择哪个？
-            `,
-          reprompt: `
-            真爱和一千万，您选择哪个？
-            `,
-        }
+        this.waitAnswer()
+        let self = this
+        return new Promise((resolve, reject) => {
+          choiceDb.find({}, (err, docs) => {
+            let doc = _.sample(docs)
+            self.setSessionAttribute('doc', doc)
+            console.log(doc);
+            let outputSpeech = `请听题。a,${doc.a}。和。b,${doc.b}。你选哪个？`
+            resolve({
+              outputSpeech: outputSpeech,
+              reprompt: '选a, 还是选b呢？',
+            })
+          })
+        })
+
       }
     });
   }
